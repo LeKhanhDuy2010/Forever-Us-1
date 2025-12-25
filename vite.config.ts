2e456780435694697f42d1e57b2893e8c53726bf
IT: Louis Duy <louis.duy@feavn.local>
2025-12-25 16:53:07 +0700

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // 👇 QUAN TRỌNG NHẤT
+      base: '/Forever-Us-1/',
       server: {
         port: 3000,
         host: '0.0.0.0',
